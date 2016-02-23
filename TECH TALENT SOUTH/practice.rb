e995b16c47e6ea89af23d645134358221641e3a6
MeganJ7 <meganjacksen@gmail.com>
2016-02-22 18:26:54 -0700

    inital commit

---
+++ TECH TALENT SOUTH/practice.rb
@@ -0,0 +1,32 @@
+# puts "Jared"
+# puts 1234
+# puts 14.65
+
+# puts 2/1.0
+
+
+ # puts "Jared" + 3
+
+# puts "Jared" * 3_f
+
+# # puts 1/2.to_f
+# a = 10
+# b = 5
+# c = 2
+# c = 1
+# puts c 
+
+my_name = 'John Smith'
+
+height_inches = 60
+
+weight_pounds = 120
+
+height_centimeters = height_inches * 2.54
+
+weight_kilograms = weight_pounds * 0.453592
+puts height_centimeters
+puts weight_kilograms
+
+puts my_name + ' is ' + height_centimeters.to_s + ' cm and ' + weight_kilograms.to_s + ' kg.'
+
---
+++ megan.rb
@@ -0,0 +1,7 @@
+ENTRY_TO_SYM = { 'p'=>:PAPER, 'r'=>:ROCK, 's'=>:SCISSORS }
+VALID_ENTRIES = ENTRY_TO_SYM.keys
+COMPUTER_CHOICES = ENTRY_TO_SYM.values
+# WINNERS and LOSERS from the player's perspective, the first value of each
+# pair being the player's choice, the second, the computer's choice.
+WINNERS = [[:SCISSORS, :PAPER], [:PAPER, :ROCK], [:ROCK, :SCISSORS]]
+LOSERS =  WINNERS.map { |i,j| [j,i] }
