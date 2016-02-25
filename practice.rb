b9d15be58d275e6696a2e4ad50ee52d75ea85781
MeganJ7 <meganjacksen@gmail.com>
2016-02-24 19:03:43 -0700

    pracice ruby 022416

--- practice.rb
+++ practice.rb
@@ -1,1 +1,20 @@
-irb(main):003:0> 3+2
+class Word
+	def very_long?(string)
+		if string.length >= 10
+			puts "true"
+		end
+	end
+end
+
+word1 = Word.new
+puts word1
+puts word1.very_long?("thisiswaytoolong")
+
+
+
+
+
+
+
+
+#student_name.each { |student, rank| puts "#{student} is #{rank} ranked"}
